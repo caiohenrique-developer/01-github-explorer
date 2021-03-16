4c7aff5ff68dc97f0585818bc5a21cca521455bf
caiohenrique-developer <caiorussellrox@msn.com>
2021-03-16 16:05:02 -0300

    :sparkles: [ FEAT ]: Create counter to study state concepts

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { Counter } from './components/Counter';
 import { RepositoryList } from './components/RepositoryList';
 
 import './styles/globals.scss';
 
 export function App() {
-    return <RepositoryList />;
+    return (
+        <>
+            <RepositoryList />
+            <Counter />
+        </>
+    );
 }
